54d5bfc73ace5bf73666e95b70377aae4d886aea
AyS <143962927+AyrtonSur@users.noreply.github.com>
2023-10-29 16:56:16 -0300

    Create 2344.py

---
+++ 2344.py
@@ -0,0 +1,11 @@
+a=int(input())
+if a==0:
+    print('E')
+elif a in range(0,35+1):
+    print('D')
+elif a in range(36,60+1):
+    print('C')
+elif a in range(61,85+1):
+    print('B')
+elif a in range(86,100+1):
+    print('A')
