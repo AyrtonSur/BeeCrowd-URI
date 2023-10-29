da0726c188754c06bd0c6e93e39ae868c8436863
AyS <143962927+AyrtonSur@users.noreply.github.com>
2023-10-29 17:15:36 -0300

    Create 1021.py

---
+++ 1021.py
@@ -0,0 +1,38 @@
+n1=float(input())+0.001
+n2=n1/100
+k1=n1%100
+n3=k1/50
+k2=k1%50
+n4=k2/20
+k3=k2%20
+n5=k3/10
+k4=k3%10
+n6=k4/5
+k5=k4%5
+n7=k5/2
+k6=k5%2
+n8=k6/1
+k7=k6%1
+n9=k7/0.5
+k8=k7%0.5
+n10=k8/0.25
+k9=k8%0.25
+n11=k9/0.10
+k10=k9%0.10
+n12=k10/0.05
+k11=k10%0.05
+n13=k11/0.01
+print('NOTAS:')
+print(f'{int(n2)} nota(s) de R$ 100.00')
+print(f'{int(n3)} nota(s) de R$ 50.00')
+print(f'{int(n4)} nota(s) de R$ 20.00')
+print(f'{int(n5)} nota(s) de R$ 10.00')
+print(f'{int(n6)} nota(s) de R$ 5.00')
+print(f'{int(n7)} nota(s) de R$ 2.00')
+print('MOEDAS:')
+print(f'{int(n8)} moeda(s) de R$ 1.00')
+print(f'{int(n9)} moeda(s) de R$ 0.50')
+print(f'{int(n10)} moeda(s) de R$ 0.25')
+print(f'{int(n11)} moeda(s) de R$ 0.10')
+print(f'{int(n12)} moeda(s) de R$ 0.05')
+print(f'{int(n13)} moeda(s) de R$ 0.01')
